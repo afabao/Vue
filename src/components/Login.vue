24466d5d34525c0575a86f410b91b029b4e08f22
afabao <454836582@qq.com>
2019-07-01 17:19:16 +0800

    Initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <input type="text" v-model="name">
+        <input type="text" v-model="age">
+        <button @click="getem">commit</button>
+    </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  const axiosInstance = axios.create({
+    headers: {'Content-Type': 'application/json;charset=utf-8'},
+    withCredentials: true
+  })
+export default {
+   data () {
+     return {
+       name:'',
+       age:''
+     }
+   },
+
+  methods: {
+     getem: function(){
+       axiosInstance({
+         method: "get",
+         url: "/api/test"
+       }).then(response => {
+         this.name = response.data[0].name;
+         this.age = response.data[0].age;
+       })
+     }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
